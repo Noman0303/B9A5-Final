c227f164db4ae34d4bad62ff1652c137b930dd05
Md. Mukaddemu Khan Noman <MNoman@rgbd.local>
2024-02-22 18:34:40 +0600

    discount applied

--- ticket-function.js
+++ ticket-function.js
@@ -54,38 +54,32 @@ function updateTotalCost(price){
 
 
 
+// coupon apply
 
-// const seatnumber = document.getElementById(seat-number).innertext
-// const class = document.getElementById(Economy).innertext;
-// const seatValue = document.getElementById(value).innertext;
+function applyCoupon() {
+    const couponCode = document.getElementById("couponInput").value;
+    if (couponCode === "NEW15") {
+        apply15()
+    }
+    else if (couponCode === "Couple 20") {
+        apply20()
+    }
 
+    else {
+      alert("Invalid coupon code. Please try again.");
+    }
+  }
 
-// // coupon apply
+  function apply15() {
+    const discountedvaluetext1=document.getElementById("Ticket-sum").innerText;
+        const discountedvalue=parseFloat(discountedvaluetext1);
+        const discountamount=discountedvalue*0.15;
+        document.getElementById("Ticket-discount").innerText = discountamount.toFixed(2);
+  }
 
-// function applyCoupon() {
-//     const couponCode = document.getElementById("couponInput").value;
-//     if (couponCode === "NEW15") {
-//         apply15()
-//     }
-//     else if (couponCode === "Couple 20") {
-//         apply20()
-//     }
-
-//     else {
-//       alert("Invalid coupon code. Please try again.");
-//     }
-//   }
-
-//   function apply15() {
-//     const discountedvaluetext1=document.getElementById("Ticket-sum").innerText;
-//         const discountedvalue=parseFloat(discountedvaluetext1);
-//         const discountamount=discountedvalue*0.15;
-//         document.getElementById("Ticket-discount").innerText = discountamount.toFixed(2);
-//   }
-
-//   function apply20() {
-//     const discountedvaluetext1=document.getElementById("Ticket-sum").innerText;
-//         const discountedvalue=parseFloat(discountedvaluetext1);
-//         const discountamount=discountedvalue*0.20;
-//         document.getElementById("Ticket-discount").innerText = discountamount.toFixed(2);
-//   }
+  function apply20() {
+    const discountedvaluetext1=document.getElementById("Ticket-sum").innerText;
+        const discountedvalue=parseFloat(discountedvaluetext1);
+        const discountamount=discountedvalue*0.20;
+        document.getElementById("Ticket-discount").innerText = discountamount.toFixed(2);
+  }
