dfeae5799783229dff71aa9d88e09d0a51571b36
Md. Mukaddemu Khan Noman <MNoman@rgbd.local>
2024-02-22 11:44:53 +0600

    first commit

---
+++ ticket-function.js
@@ -0,0 +1,48 @@
+function getConvertedValue(id){
+    const selectedItemString = document.getElementById(id).innerText;
+    const selectedItemValue = parseInt(selectedItemString)
+    return selectedItemValue
+}
+
+const allbtn=document.getElementsByClassName(btn-selected)
+
+for (const btn of allbtn) {
+    btn.addEventListener("click",function(event){
+        console.log(event)
+    })
+}
+
+const seatnumber = document.getElementById(seat-number).innertext
+const class = document.getElementById(Economy).innertext;
+const seatValue = document.getElementById(value).innertext;
+
+
+// coupon apply
+
+function applyCoupon() {
+    const couponCode = document.getElementById("couponInput").value;
+    if (couponCode === "NEW15") {
+        apply15()
+    }
+    else if (couponCode === "Couple 20") {
+        apply20()
+    }
+
+    else {
+      alert("Invalid coupon code. Please try again.");
+    }
+  }
+
+  function apply15() {
+    const discountedvaluetext1=document.getElementById("Ticket-sum").innerText;
+        const discountedvalue=parseFloat(discountedvaluetext1);
+        const discountamount=discountedvalue*0.15;
+        document.getElementById("Ticket-discount").innerText = discountamount.toFixed(2);
+  }
+
+  function apply20() {
+    const discountedvaluetext1=document.getElementById("Ticket-sum").innerText;
+        const discountedvalue=parseFloat(discountedvaluetext1);
+        const discountamount=discountedvalue*0.20;
+        document.getElementById("Ticket-discount").innerText = discountamount.toFixed(2);
+  }
